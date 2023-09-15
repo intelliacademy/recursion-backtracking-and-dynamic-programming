7ee890ca0a11c5d0a31cfe82a92240c0f684122c
Vuqar Mammadli <vuqar.mammadli@cybernet.az>
2023-09-15 17:32:17 +0400

    introduction

--- src/main/java/com/intelliacademy/lesson/Main.java
+++
@@ -1,7 +0,0 @@
-package com.intelliacademy.lesson;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/com/intelliacademy/lesson/package-info.java
@@ -0,0 +1,1 @@
+package com.intelliacademy.lesson;
