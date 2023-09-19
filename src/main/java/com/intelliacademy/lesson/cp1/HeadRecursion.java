e8e4893e1b246e6bb91a6166baf6350da0542b08
Vuqar Mammadli <vuqar.mammadli@cybernet.az>
2023-09-19 10:20:47 +0400

    introduction

---
+++ src/main/java/com/intelliacademy/lesson/cp1/HeadRecursion.java
@@ -0,0 +1,11 @@
+package com.intelliacademy.lesson.cp1;
+
+public class HeadRecursion {
+    /**
+     *  If the recursion function call occurs at the beginning of the function then
+     *  it is called tail recursion
+     */
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/intelliacademy/lesson/cp1/TailRecursion.java
@@ -0,0 +1,11 @@
+package com.intelliacademy.lesson.cp1;
+
+public class TailRecursion {
+    /**
+     *  If the recursion function call occurs at the end of the function then
+     *  it is called tail recursion
+     */
+    public static void main(String[] args) {
+
+    }
+}
