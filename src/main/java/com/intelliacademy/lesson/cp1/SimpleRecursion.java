d32e51785b76ed6c62637a7f109ce3e4c192a6e7
Vuqar Mammadli <vuqar.mammadli@cybernet.az>
2023-09-19 10:16:59 +0400

    introduction

---
+++ src/main/java/com/intelliacademy/lesson/cp1/SimpleRecursion.java
@@ -0,0 +1,13 @@
+package com.intelliacademy.lesson.cp1;
+
+public class SimpleRecursion {
+    public static void main(String[] args) {
+        var result = sum(100);
+        System.out.println(result);
+    }
+
+    public static int sum(int n){
+        if(n == 0) return 0;
+        else return n + sum(n - 1);
+    }
+}
--- src/main/java/com/intelliacademy/lesson/cp1/StackFrameCalling.java
+++ src/main/java/com/intelliacademy/lesson/cp1/StackFrameCalling.java
@@ -1,6 +1,6 @@
 package com.intelliacademy.lesson.cp1;
 
-public class Recursion {
+public class StackFrameCalling {
     public void func1(){
         System.out.println("func1() executed");
         this.func2();
@@ -23,7 +23,7 @@ public class Recursion {
 
 class Main {
     public static void main(String[] args) {
-        Recursion recursion = new Recursion();
-        recursion.func1();
+        StackFrameCalling stackFrameCalling = new StackFrameCalling();
+        stackFrameCalling.func1();
     }
 }
