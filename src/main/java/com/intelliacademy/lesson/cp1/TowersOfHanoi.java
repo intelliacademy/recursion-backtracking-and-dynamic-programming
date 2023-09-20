5587d2c2a58e4d81eb36c0ff9c702ea657a87591
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-09-20 23:12:35 +0400

    .

---
+++ src/main/java/com/intelliacademy/lesson/cp1/TowersOfHanoi.java
@@ -0,0 +1,25 @@
+package com.intelliacademy.lesson.cp1;
+
+public class TowersOfHanoi {
+    /**
+     * n is the number of disks and source, destination and auxiliary are the names of the rods.
+     * @param n number of disks . n = 1 is the base case and smallest disk.
+     */
+    public void solve(int n,char source,char destination,char auxiliary){
+        if(n==1){
+            System.out.println("Move disk 1 from "+source+" to "+destination);
+            return;
+        }
+        solve(n-1,source,auxiliary,destination);
+        System.out.println("Move disk "+n+" from "+source+" to "+destination);
+        solve(n-1,auxiliary,destination,source);
+    }
+}
+
+class TowersOfHanoiMain{
+    public static void main(String[] args) {
+        TowersOfHanoi towersOfHanoi=new TowersOfHanoi();
+        towersOfHanoi.solve(10,'A','B','C');
+    }
+}
+
