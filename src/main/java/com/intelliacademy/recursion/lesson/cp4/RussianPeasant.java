18209b4bddbdb70b886cae6b4c34923eba3c5d3c
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-09-22 23:54:13 +0400

    .

--- src/main/java/com/intelliacademy/recursion/lesson/cp4/RussianPeasant.java
+++ src/main/java/com/intelliacademy/recursion/lesson/cp4/RussianPeasant.java
@@ -1,7 +1,20 @@
 package com.intelliacademy.recursion.lesson.cp4;
 
 public class RussianPeasant {
+    private final EvenAndOdd evenAndOdd = new EvenAndOdd();
+    public int multiply(int a, int b){
+        if(a == 0)
+            return 0;
+        else if(this.evenAndOdd.isEven(a))
+            return multiply(a/2, b*2);
+        else
+            return b + multiply(a/2, b*2);
+    }
 }
 class RussianPeasantTest{
+    public static void main(String[] args) {
+        RussianPeasant russianPeasant = new RussianPeasant();
+        System.out.println(russianPeasant.multiply(50, 50));
+    }
 
 }
