86e1c0252f2b5ba15911bd64e449f1938f16148a
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-09-23 19:18:57 +0400

    .

---
+++ src/main/java/com/intelliacademy/recursion/lesson/cp5/HamiltonianCycleProblem.java
@@ -0,0 +1,13 @@
+package com.intelliacademy.recursion.lesson.cp5;
+
+public class HamiltonianCycleProblem {
+
+}
+
+class HamiltonianCycleProblemTest{
+    public static void main(String[] args) {
+        HamiltonianCycleProblem hamiltonianCycleProblem = new HamiltonianCycleProblem();
+
+    }
+
+}
