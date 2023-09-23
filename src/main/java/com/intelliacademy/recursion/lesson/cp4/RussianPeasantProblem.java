4049ce650171b1104ed1e924a55ffea98bc9b6e6
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-09-23 17:11:22 +0400

    .

--- src/main/java/com/intelliacademy/recursion/lesson/cp4/RussianPeasantProblem.java
+++ src/main/java/com/intelliacademy/recursion/lesson/cp4/RussianPeasantProblem.java
@@ -6,17 +6,16 @@ public class RussianPeasantProblem {
     /**
      * This method multiplies two numbers using Russian Peasant Algorithm
      * <br>
-     * a * b = 2*a * b/2 if a is even
+     * a * b = 2*a * b/2 if b is even
      * <br>
-     * a * b = 2*a * b/2 + a if a is odd
+     * a * b = 2*a * b/2 + a if b is odd
      */
     public int multiply(int a, int b){
-        if(a == 0)
-            return 0;
-        else if(this.evenAndOddProblem.isEven(a))
-            return multiply(a/2, b*2);
+        if(a == 0 || b == 0) return 0;
+        else if(this.evenAndOddProblem.isEven(b))
+            return multiply(2*a, b/2);
         else
-            return b + multiply(a/2, b*2);
+            return a + multiply(2*a, b/2);
     }
 }
 class RussianPeasantTest{
