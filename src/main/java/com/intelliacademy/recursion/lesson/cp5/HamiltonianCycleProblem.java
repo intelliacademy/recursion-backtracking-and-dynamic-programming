ff68a8c8bcf34696598b93c7c99420fa644dca1d
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2023-09-28 22:45:58 +0400

    .

--- src/main/java/com/intelliacademy/recursion/lesson/cp5/HamiltonianCycleProblem.java
+++ src/main/java/com/intelliacademy/recursion/lesson/cp5/HamiltonianCycleProblem.java
@@ -1,12 +1,39 @@
 package com.intelliacademy.recursion.lesson.cp5;
 
 public class HamiltonianCycleProblem {
+    private int vertices;
+    private int[] hamiltonianPath;
+
+    private int[][] adjacencyMatrix;
+
+    public HamiltonianCycleProblem(int[][] adjacencyMatrix) {
+        this.vertices = adjacencyMatrix[0].length;
+        this.hamiltonianPath = new int[this.vertices];
+        this.hamiltonianPath[0] = 0;
+        this.adjacencyMatrix = adjacencyMatrix;
+    }
+
+
+    public void solve() {
+        if (findFeasibleSolution(1)) {
+            showHamiltonianPath();
+        } else {
+            System.out.println("No feasible solution...");
+        }
+    }
+
+    private void showHamiltonianPath() {
+
+    }
+
+    private boolean findFeasibleSolution(int i) {
+        return false;
+    }
 
 }
 
 class HamiltonianCycleProblemTest{
     public static void main(String[] args) {
-        HamiltonianCycleProblem hamiltonianCycleProblem = new HamiltonianCycleProblem();
 
     }
 
